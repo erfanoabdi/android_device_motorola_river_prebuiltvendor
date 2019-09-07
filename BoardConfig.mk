6c961f111edc42c7fe86ce269be609135c2eaf29
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-06 19:56:16 -0700

    river: Define paths to DTC and MKDTIMG
    These are no longer on PATH, so we have to define them explicitly
    for the kernel make system to find them.
    
    Change-Id: I69f1a01f2f3d4c90e5a2337f44ee8e1bcb1cc1e8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,6 +55,9 @@ TARGET_KERNEL_CONFIG := river_defconfig
 TARGET_KERNEL_VERSION := 4.9
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+    DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc \
+    MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
 
 # Declare boot header 
 BOARD_BOOT_HEADER_VERSION := 1
